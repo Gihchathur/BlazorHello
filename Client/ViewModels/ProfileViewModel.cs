337a8baffbc7fb79484dd89003fcc635153e435d
Gihan Chathuranga <Gihan.C@CreativeSoftware.com>
2021-11-04 12:01:55 +0530

    firstcommit

---
+++ Client/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+namespace HelloBlazor.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public long UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+        public string Message { get; set; }
+
+
+        
+
+
+    }
+
+
+}
+
---
+++ Shared/UserDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloBlazor.Shared
+{
+    public class UserDetails
+    {
+        public int Contactid { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public UserDetails()
+        {
+
+        }
+
+
+        public UserDetails(int contactId, string firstName, string lastName)
+        {
+            this.Contactid = contactId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public UserDetails(int contactId, string firstName, string lastName, string email)
+        {
+            this.Contactid = contactId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Email = email;
+        }
+    }
+}
